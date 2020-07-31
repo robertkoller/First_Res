396ef825c8a94f006ebfdde6c01c8018c56648a7
Robert_K <robertdkoller@hotmail.com>
2020-07-30 18:05:15 -0700

    Create Exercise 3.7 for Class 5.py

---
+++ Exercise 3.7 for Class 5.py
@@ -0,0 +1,8 @@
+x = input("tell me a word")
+if x[0] >= "A" and x[0] <= "Z":
+    print("your first letter is an uppercase")
+else:
+    print("your first letter is not an uppercase")
+
+#for x in range(len(x)):
+    
